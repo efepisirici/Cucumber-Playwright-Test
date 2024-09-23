338401832e9feb3fa4185fc62a1179854e842cb3
efepisirici <34378599+efepisirici@users.noreply.github.com>
2024-09-23 09:56:21 +0300

    Update RunCucumberTest.java

--- src/test/java/main/RunCucumberTest.java
+++ src/test/java/main/RunCucumberTest.java
@@ -4,13 +4,13 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-@RunWith( Cucumber.class )
+@RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features/desktop/TechCrunchDesktop.feature",
+        features = "src/test/resources/features/desktop/", 
         glue= {"steps"},
         plugin = {"pretty", "html:target/cucumber.html"},
         monochrome = true
-        )
+)
 public class RunCucumberTest {
 
 }
