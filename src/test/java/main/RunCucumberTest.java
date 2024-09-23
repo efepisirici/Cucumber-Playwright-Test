0202bdc77cb95ff799945a4ba83d8d174e6a1032
efepisirici <34378599+efepisirici@users.noreply.github.com>
2024-09-23 09:45:53 +0300

    Update RunCucumberTest.java

--- src/test/java/main/RunCucumberTest.java
+++ src/test/java/main/RunCucumberTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith( Cucumber.class )
 @CucumberOptions(
-        features = "src/test/resources/features/desktop",
+        features = "src/test/resources/features/desktop/TechCrunchDesktop.feature",
         glue= {"steps"},
         plugin = {"pretty", "html:target/cucumber.html"},
         monochrome = true
